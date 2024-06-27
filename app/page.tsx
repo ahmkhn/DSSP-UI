1818af405dce46205dd5ee921a5d99a6814dd9f2
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2024-06-27 19:51:24 -0400

    misc

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,13 @@ export default function Home() {
       </div>
       <div className="flex flex-col justify-center bg-[#a2a8d3] h-[95vh]">
         <div className="flex justify-center flex-grow">
-          <MapContainer className="w-[80%] rounded-[0.25rem] mt-10" center={[31.3909,74.2417]} zoom={10}>
+          <MapContainer className="w-[80%] rounded-[0.25rem] mt-10" center={[31.3909,74.2417]} zoom={13}>
                 /<TileLayer
                 attribution="Google Maps"
                 //url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}" // regular
                 url="http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}" // satellite
                 //url="http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}" // terrain
+                //url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
                 maxZoom={20}
                 subdomains={["mt0", "mt1", "mt2", "mt3"]}
                 />*/
