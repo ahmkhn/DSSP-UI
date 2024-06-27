6d9b6309ee9c0e91145aa0bc4f23d8aa9b82935f
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2024-06-27 18:01:56 -0400

    testing leaflet with map component

---
+++ app/Components/leafletmap.tsx
@@ -0,0 +1,12 @@
+
+import { MapContainer } from "react-leaflet";
+export default function LeafletMap() {
+    return (
+      <div>
+        <MapContainer>
+
+        </MapContainer>
+      </div>
+    );
+  }
+  
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-
+'use client';
+import LeafletMap from "./Components/leafletmap";
+import { MapContainer } from "react-leaflet";
 export default function Home() {
   return (
     <div className="h-[90vh]">
       <title>DSSP</title>
       <h1 className="text-center mt-2">Decolonizing Social Sciences in Pakistan</h1>
+      <LeafletMap>
+        
+      </LeafletMap>
     </div>
   );
 }
