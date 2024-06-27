8398bd6851e3c796608426357c8849248b50e26e
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2024-06-27 19:18:30 -0400

    header/footer

---
+++ app/Components/Header.tsx
@@ -0,0 +1,15 @@
+
+import { Toolbar } from 'primereact/toolbar';
+
+export default function Header() {
+    return(
+        <>
+            <div className="bg-[#e7eaf6] w-[100%]">
+                <Toolbar className="bg-[#e7eaf6] mr-5 ml-5 h-[2rem] text-center" center="Decolonizing Social Sciences in Pakistan" end="About" start="DSSP"></Toolbar>
+            </div>
+            
+        </>
+    );
+
+
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,30 @@
 'use client';
+import Header from "./Components/Header";
 import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 export default function Home() {
   return (
     <>
-      <div>
-        <title>DSSP</title>
-        <h1 className="text-center mt-2 mb-2 text-3xl">Decolonizing Social Sciences in Pakistan</h1>
+      <title>DSSP</title>
+      <div className="flex justify-center mb-2">
+        <Header/>
       </div>
-      <div className="flex justify-center">
-          <MapContainer className="h-[80vh] max-h-[90vh] w-[80%] rounded-[0.25rem]" center={[31.3909,74.2417]} zoom={12.5}>
-              /<TileLayer
-              attribution="Google Maps"
-              //url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}" // regular
-              url="http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}" // satellite
-              //url="http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}" // terrain
-              maxZoom={20}
-              subdomains={["mt0", "mt1", "mt2", "mt3"]}
-              />*/
+      <div className="flex flex-col justify-center bg-[#a2a8d3] h-[95vh]">
+        <div className="flex justify-center flex-grow">
+          <MapContainer className="w-[80%] rounded-[0.25rem] mt-10" center={[31.3909,74.2417]} zoom={12.5}>
+                /<TileLayer
+                attribution="Google Maps"
+                //url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}" // regular
+                url="http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}" // satellite
+                //url="http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}" // terrain
+                maxZoom={20}
+                subdomains={["mt0", "mt1", "mt2", "mt3"]}
+                />*/
           </MapContainer>
+        </div>
+        <div>
+          <footer className="text-center h-[1rem] text-xs bg-white">developed by @ahmkhn</footer>
+        </div>
       </div>
     </>
   );
