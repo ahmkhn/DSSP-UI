fcbf258df9b3baf018cdc999ad071369bfc4a347
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2024-06-27 18:10:03 -0400

    beginning setting up the map

--- app/Components/leafletmap.tsx
+++ app/Components/leafletmap.tsx
@@ -1,12 +1,15 @@
-
-import { MapContainer } from "react-leaflet";
+'use client';
+import { MapContainer, TileLayer } from "react-leaflet";
 export default function LeafletMap() {
     return (
       <div>
-        <MapContainer>
-
+        <MapContainer center={[31.5204,74.3587]} zoom={13}>
+        <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
         </MapContainer>
       </div>
     );
-  }
+}
   
